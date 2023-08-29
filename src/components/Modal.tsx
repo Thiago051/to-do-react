56e68e4891f22e90e3cf1de216dbc7865a99576d
Thiago Gabriel <thiago.gabriel051@gmail.com>
2023-08-29 19:25:40 -0300

    Criando Modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,17 @@
+// css
+import styles from './Modal.module.css'
+
+type Props = {}
+
+const Modal = ({ }: Props) => {
+    return (
+        <div id="modal">
+            <div></div>
+            <div>
+                <h2>Texto modal</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
